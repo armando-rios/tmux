1ef35a6bea783bba471c0c119b63b8bc985ddd3e
Armando Rios <armandorios.dev@gmail.com>
2025-01-01 22:28:01 -0800

    test: work?

---
+++ plugin.tmux
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# source and run plugin
+
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$current_dir/scripts/plugin.sh
---
+++ scripts/plugin.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# setting the locale, some users have issues with different locales, this forces the correct one
+export LC_ALL=en_US.UTF-8
+
+main() 
+{
+  # Catppuccin Mocha Colors (oficiales)
+  rosewater="#F5E0DC"
+  flamingo="#F2CDCD"
+  pink="#F5C2E7"
+  mauve="#CBA6F7"
+  red="#F38BA8"
+  maroon="#EBA0AC"
+  peach="#FAB387"
+  yellow="#F9E2AF"
+  green="#A6E3A1"
+  teal="#94E2D5"
+  sky="#89DCEB"
+  sapphire="#74C7EC"
+  blue="#89B4FA"
+  lavender="#B4BEFE"
+  text="#CDD6F4"
+  subtext1="#BAC2DE"
+  subtext0="#A6ADC8"
+  overlay2="#9399B2"
+  overlay1="#7F849C"
+  overlay0="#6C7086"
+  surface2="#585B70"
+  surface1="#45475A"
+  surface0="#313244"
+  base="#1E1E2E"
+  mantle="#181825"
+  crust="#11111B"
+  background="#1e1e2e"
+
+  tmux set -g status-left "#[fg=#ff79c6]#S #[fg=white]⏎"
+  tmux set -g status-right "#[fg=#ff79c6]#H #[fg=white]⏎"
+}
+main
