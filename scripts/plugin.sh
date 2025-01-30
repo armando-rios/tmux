a274927b86afc148ca34468ea9edac788ced2f83
Armando Rios <armandorios.dev@gmail.com>
2025-01-29 19:10:44 -0800

    feat: add blank space on cwd

--- scripts/plugin.sh
+++ scripts/plugin.sh
@@ -37,7 +37,7 @@ set_status_right() {
   status_right=""
 
   if [[ "$show_cwd" == "on" ]]; then
-    status_right+="#[fg=${colors[pink]},bg=${colors[background]}]${sep_right}#[fg=${colors[surface0]},bg=${colors[pink]}]  #(sh ${current_dir}/cwd.sh)"
+    status_right+="#[fg=${colors[pink]},bg=${colors[background]}]${sep_right}#[fg=${colors[surface0]},bg=${colors[pink]}]  #(sh ${current_dir}/cwd.sh) "
   fi
   
   if [[ "$show_clock" == "on" ]]; then
