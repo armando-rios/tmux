dcc4d608675cfe8b0f3d9331e64bc403242fdff9
Armando Rios <armandorios.dev@gmail.com>
2025-01-29 17:55:48 -0800

    fix: systax error on status left module

--- scripts/plugin.sh
+++ scripts/plugin.sh
@@ -4,6 +4,7 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Cargar colores desde archivo externo
 source "${current_dir}/colors.sh"
+echo "${colors[background]}"
 
 # Símbolos y separadores
 sep_left=""
@@ -18,13 +19,12 @@ set_status_bar() {
 }
 
 set_status_left() {
-  prefix_style="#{?client_prefix,#[bg=${colors[green]}],}"
-  tmux set-option -g status-left "#[bg=${colors[blue]},fg=${colors[background]}]${prefix_style} ${left_icon} #H #[fg=${colors[blue}],bg=${colors[background]}]${sep_left}"
+  tmux set-option -g status-left "#[bg=${colors[blue]},fg=${colors[background]}]#{?client_prefix,#[bg=${colors[green]}],} ${left_icon} #H #[fg=${colors[blue]},bg=${colors[background]}]#{?client_prefix,#[fg=${colors[green]}],}${sep_left} "
 }
 
 set_window_options() {
   tmux set-window-option -g window-status-current-format "#[fg=${colors[red]},bg=${colors[background]}]${divider}#[fg=${colors[surface0]},bg=${colors[red]}] ${on_icon}  #W #[fg=${colors[red]},bg=${colors[background]}]${sep_left}"
-  tmux set-window-option -g window-status-format "#[fg=${colors[mauve]},bg=${colors[background]}]${divider}#[fg=${colors[surface0]},bg=${colors[mauve]}] ${off_icon}  #W #[fg=${colors[mauve}],bg=${colors[background]}]${sep_left}"
+  tmux set-window-option -g window-status-format "#[fg=${colors[mauve]},bg=${colors[background]}]${divider}#[fg=${colors[surface0]},bg=${colors[mauve]}] ${off_icon}  #W #[fg=${colors[mauve]},bg=${colors[background]}]${sep_left}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
 }
