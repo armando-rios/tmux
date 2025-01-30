5e166acdddf93b5b105f9f0939ba55e93787c7c8
Armando Rios <armandorios.dev@gmail.com>
2025-01-29 16:50:57 -0800

    feat: change to modularized version:

--- scripts/plugin.sh
+++ scripts/plugin.sh
@@ -1,68 +1,71 @@
 #!/usr/bin/env bash
-# setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
-
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-main()
-{
-  # Catppuccin Mocha Colors (oficiales)
-  rosewater="#F5E0DC"
-  flamingo="#F2CDCD"
-  pink="#F5C2E7"
-  mauve="#CBA6F7"
-  red="#F38BA8"
-  maroon="#EBA0AC"
-  peach="#FAB387"
-  yellow="#F9E2AF"
-  green="#A6E3A1"
-  teal="#94E2D5"
-  sky="#89DCEB"
-  sapphire="#74C7EC"
-  blue="#89B4FA"
-  lavender="#B4BEFE"
-  text="#CDD6F4"
-  subtext1="#BAC2DE"
-  subtext0="#A6ADC8"
-  overlay2="#9399B2"
-  overlay1="#7F849C"
-  overlay0="#6C7086"
-  surface2="#585B70"
-  surface1="#45475A"
-  surface0="#313244"
-  base="#1E1E2E"
-  mantle="#181825"
-  crust="#11111B"
-  background="#1E1E2E"
+# Colores Catppuccin Mocha
+declare -A colors=(
+  [rosewater]="#F5E0DC"
+  [flamingo]="#F2CDCD"
+  [pink]="#F5C2E7"
+  [mauve]="#CBA6F7"
+  [red]="#F38BA8"
+  [maroon]="#EBA0AC"
+  [peach]="#FAB387"
+  [yellow]="#F9E2AF"
+  [green]="#A6E3A1"
+  [teal]="#94E2D5"
+  [sky]="#89DCEB"
+  [sapphire]="#74C7EC"
+  [blue]="#89B4FA"
+  [lavender]="#B4BEFE"
+  [text]="#CDD6F4"
+  [subtext1]="#BAC2DE"
+  [subtext0]="#A6ADC8"
+  [overlay2]="#9399B2"
+  [overlay1]="#7F849C"
+  [overlay0]="#6C7086"
+  [surface2]="#585B70"
+  [surface1]="#45475A"
+  [surface0]="#313244"
+  [base]="#1E1E2E"
+  [mantle]="#181825"
+  [crust]="#11111B"
+  [background]="#1E1E2E"
+)
 
-  sep_left=""
-  sep_right=""
-  divider=""
-  left_icon=""
-  on_icon=""
-  off_icon=""
+# Símbolos y separadores
+sep_left=""
+sep_right=""
+divider=""
+left_icon=""
+on_icon=""
+off_icon=""
 
-  # Status bar
-  tmux set-option -g status-style "bg=${background},fg=${text}"
-
-  # Status left
-  tmux set-option -g status-left "#[bg=${blue},fg=${base}]#{?client_prefix,#[bg=${green}],} ${left_icon} #H #[fg=${blue},bg=${background}]#{?client_prefix,#[fg=${green}],}${sep_left} "
-
-  # Window option on
-  tmux set-window-option -g window-status-current-format "#[fg=${red},bg=${background}]${divider}#[fg=${surface0},bg=${red}]   #W #[fg=${red},bg=${background}]${sep_left}"
-
-  # Window option off
-  tmux set-window-option -g window-status-format "#[fg=${mauve},bg=${background}]${divider}#[fg=${surface0},bg=${mauve}]   #W #[fg=${mauve},bg=${background}]${sep_left}"
-
-  tmux set-window-option -g window-status-activity-style "bold"
-  tmux set-window-option -g window-status-bell-style "bold"
-
-  # Status right
-  tmux set-option -g status-right ""
-
-  # cwd
-  tmux set-option -g status-right "#[fg=${pink},bg=${background}]${sep_right}#[fg=${surface0},bg=${pink}]  #(${current_dir}/cwd.sh) "
+set_status_bar() {
+  tmux set-option -g status-style "bg=${colors[background]},fg=${colors[text]}"
 }
 
-# run main function
+set_status_left() {
+  prefix_style="#{?client_prefix,#[bg=${colors[green]}],}"
+  tmux set-option -g status-left "#[bg=${colors[blue]},fg=${colors[background]}]${prefix_style} ${left_icon} #H #[fg=${colors[blue}],bg=${colors[background]}]${sep_left}"
+}
+
+set_window_options() {
+  tmux set-window-option -g window-status-current-format "#[fg=${colors[red]},bg=${colors[background]}]${divider}#[fg=${colors[surface0]},bg=${colors[red]}] ${on_icon}  #W #[fg=${colors[red]},bg=${colors[background]}]${sep_left}"
+  tmux set-window-option -g window-status-format "#[fg=${colors[mauve]},bg=${colors[background]}]${divider}#[fg=${colors[surface0]},bg=${colors[mauve]}] ${off_icon}  #W #[fg=${colors[mauve]},bg=${colors[background]}]${sep_left}"
+  tmux set-window-option -g window-status-activity-style "bold"
+  tmux set-window-option -g window-status-bell-style "bold"
+}
+
+set_status_right() {
+  tmux set-option -g status-right ""
+}
+
+main() {
+  set_status_bar
+  set_status_left
+  set_window_options
+  set_status_right
+}
+
 main
