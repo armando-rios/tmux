14b89ebf366152891fc023bbae43888f2d5cec30
Armando Rios <armandorios.dev@gmail.com>
2025-02-22 17:08:01 -0800

    refactor: raname color variables to semantic funtion-based names
    - Replace sequential names with function-based names
    - Rename colors based on their UI role (hostname, active, etc.)
    - Prepare for future modular status bar components

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,31 +1,14 @@
 #!/usr/bin/env bash
 
 declare -A colors=(
-  [rosewater]="#F5E0DC"
-  [flamingo]="#F2CDCD"
-  [pink]="#F5C2E7"
-  [mauve]="#CBA6F7"
-  [red]="#F38BA8"
-  [maroon]="#EBA0AC"
-  [peach]="#FAB387"
-  [yellow]="#F9E2AF"
-  [green]="#A6E3A1"
-  [teal]="#94E2D5"
-  [sky]="#89DCEB"
-  [sapphire]="#74C7EC"
-  [blue]="#89B4FA"
-  [lavender]="#B4BEFE"
-  [text]="#CDD6F4"
-  [subtext1]="#BAC2DE"
-  [subtext0]="#A6ADC8"
-  [overlay2]="#9399B2"
-  [overlay1]="#7F849C"
-  [overlay0]="#6C7086"
-  [surface2]="#585B70"
-  [surface1]="#45475A"
-  [surface0]="#313244"
-  [base]="#1E1E2E"
-  [mantle]="#181825"
-  [crust]="#11111B"
-  [background]="#1E1E2E"
+  [base]="#1E1E2E" # base
+  [text]="#CDD6F4" # white
+  [hostname]="#89B4FA" # blue
+  [prefix]="#A6E3A1" # green
+  [active]="#F38BA8" # red
+  [inactive]="#CBA6F7" # mauve
+  [cwd]="#F5C2E7" # pink
+  [sysinfo]="#74C7EC" # sapphire
+  [clock]="#B4BEFE" # lavender
+  [background]="#1E1E2E" # base
 )
